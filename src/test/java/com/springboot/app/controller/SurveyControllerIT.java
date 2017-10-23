777dcce21d5b3936362160e9fe0d7cb6eb13affa
Lechu67 <l.szajnowski@gmail.com>
2017-10-23 18:29:02 +0200

    add first test

--- src/test/java/com/springboot/app/controller/SurveyControllerIT.java
+++ src/test/java/com/springboot/app/controller/SurveyControllerIT.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
@@ -19,6 +20,12 @@ public class SurveyControllerIT {
 
     @Test
     public void test(){
-        System.out.println("Port "+ port);
+
+
+        String url = "http://localhost:"+port+"/surveys/Survey1/questions/Question1";
+        TestRestTemplate restTemplate = new TestRestTemplate();
+        String output = restTemplate.getForObject(url,String.class);
+        assertEquals("<Question><id>Question1</id><description>Largest Country in the World</description><correctAnswer>Russia</correctAnswer><options><options>India</options><options>Russia</options><options>United States</options><options>China</options></options></Question>",output);
+        System.out.println("Response "+ output);
     }
 }
